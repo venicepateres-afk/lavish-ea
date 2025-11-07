2fb7a3c102a491dee43ec78da53c11d6eab9338e
venicepateres-afk <venicepateres@gmail.com>
2025-11-08 00:11:18 +0800

    Add files via upload

---
+++ about.php
@@ -0,0 +1,104 @@
+<?php include 'assets/includes/header.php' ?>
+<title>About Us | Saint Francis of Assisi College Bacoor</title>
+</head>
+
+<body>
+
+<?php
+ include ('assets/includes/navbar.php')
+ ?>
+
+  <main id="main">
+    <!-- ======= Breadcrumbs ======= -->
+    <div class="breadcrumbs" data-aos="fade-in">
+      <div class="container">
+        <h2>About Us</h2>
+      </div>
+    </div><!-- End Breadcrumbs -->
+
+    <!-- ======= About Section ======= -->
+    <section id="about" class="about">
+      <div class="container" data-aos="fade-up">
+
+        <div class="row">
+          <div class="col-lg-6 order-1 order-lg-2" data-aos="fade-left" data-aos-delay="100">
+            <img src="assets/img/school/1.jpg" class="d-block w-100 img-fluid" alt="...">
+            <img src="assets/img/school/2.jpg" class="d-block w-100 img-fluid" alt="...">
+            <img src="assets/img/school/3.jpg" class="d-block w-100 img-fluid" alt="...">
+          </div>
+          <div class="col-lg-6 pt-4 pt-lg-0 order-2 order-lg-1 content">
+            <div class="section-title">
+              <h2>About our</h2>
+              <p>School</p>
+            </div>
+            <!-- <p class="fst-italic">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+              magna aliqua.
+            </p>
+            <ul>
+              <li><i class="bi bi-check-circle"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat.</li>
+              <li><i class="bi bi-check-circle"></i> Duis aute irure dolor in reprehenderit in voluptate velit.</li>
+              <li><i class="bi bi-check-circle"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate trideta storacalaperda mastiro dolore eu fugiat nulla pariatur.</li>
+            </ul> -->
+            <p align="justify" style="text-indent: 25px;">
+            Saint Francis of Assisi College had its beginnings in 1981, when the husband-and-wife team of Dr. Evangeline Oñas-Orosco and Dr. Arturo Agapito Orosco, Sr., opened a two-classroom preparatory school in Talon III, Las Piñas City to provide affordable quality education for young children. By 1983, the school’s popularity was such that a two-storey, full elementary school building had to be constructed to meet the needs of the surrounding community. The opening of the high school department quickly followed in 1986, ensuring placements for SFAC graduates at the secondary level. At the same time, two new campuses were established, one in Taguig City, and one in Los Baños, Laguna.
+            </p>
+            <p align="justify" style="text-indent: 25px;">
+            In 1989, the tertiary level in SFAC came into being offering several courses both in degree and non-degree programs. New campuses have since been added: the Pamplona, Las Piñas campus in 1990, the Bacoor, Cavite campus in 1994, the Alabang, Muntinlupa campus in 1996. In the same year, 1996, Saint Anthony School of Las Piñas came under SFAC supervision and management. Dasmariñas, Cavite campus opened in 1999. The Biñan campus, in Laguna opened in 2006. In 2010, the Los Baños campus was renamed Saint Anthony School of Los Baños, named after the patron saint of the locality. The 10th campus, located in Sta. Rosa, Laguna accepts enrollees starting S.Y. 2012 – 2013. In line with corporate branding moves, the Pamplona campus, being the smallest campus, moved its operation to the main campus in 2014.
+            </p>
+            <p align="justify" style="text-indent: 25px;">
+            Dr. Arturo O. Orosco, Jr., succeeding his mother as school president in 2012, steered the school system into a brand where bayanihan spirit cultivates the deep heritage of the school. Bold moves and decisions are continuously being made to meet the new leadership’s branding innovations and to meet the founders’ objective of serving more communities, and societies, at large.
+            </p>
+            <p align="justify" style="text-indent: 25px;">
+            Today, Saint Francis of Assisi College offers complete education from preschool up to the graduate level of education.
+            </p>
+            <p align="justify" style="text-indent: 25px;">
+            In its more than two decades of existence, SFAC, recognized by the Department of Education (DepEd), Technical Education and Skills Development Authority (TESDA) and by the Commission on Higher Education (CHED), has pursued programs of instruction grounded on academic, cultural and sports excellence and tempered by Christian and Franciscan values exemplified by its patron, Saint Francis of Assisi. The Bureau of Immigration has granted SFAC the authority to accept foreign students.
+            </p>
+            <p align="justify" style="text-indent: 25px;">
+            A rich and responsive curriculum, a corps of well-trained, experienced and caring teachers, dynamic administrators, and an atmosphere conducive to excellence — all make up the lifelong learning experiences at SFAC.
+            </p>
+            <p align="justify" style="text-indent: 25px;">
+            To keep abreast with the rapid growth and changes in the educational trends in the country and elsewhere, SFAC established its Center for Excellence in Academics, Research and Training Services (Centrex) which serves as the standard-setting entity of its academic operations. Committed in providing a value-laden, excellent education, the Centrex focuses on seven (7) areas: Curriculum Development, Textbook Evaluation, Institutional Examinations, Review of Academic Policies and Procedures, Faculty Training and Ranking, Organization/Leadership Development, and Institutional Research. The Centrex ensures that uniform standards are observed and maintained in all SFAC campuses. Aside from the Centrex, a Central Office (C.O.) and a Central Purchasing (C.P.) unit likewise guarantee the operational stability of SFAC for the benefit of its employees, suppliers, students and other stakeholders.
+            </p>
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End About Section -->
+
+    <!-- ======= Why Us Section ======= -->
+    <section id="why-us" class="why-us">
+      <div class="container" data-aos="fade-up">
+        <div class="row">
+          <div class="col-lg-12 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="100">
+            <div class="icon-boxes d-flex flex-column justify-content-center">
+              <div class="row">
+                <div class="col-xl-4 d-flex align-items-stretch">
+                  <div class="icon-box mt-4 mt-xl-0">
+                    <h4>VISION</h4>
+                    <p align="justify" style="text-indent: 25px;">Saint Francis of Assisi College (SFAC) aims to be an exemplar for progressive holistic education, blending academic excellence with physical, spiritual, and cultural values.</p>
+                  </div>
+                </div>
+                <div class="col-xl-4 d-flex align-items-stretch">
+                  <div class="icon-box mt-4 mt-xl-0">
+                    <h4>MISSION</h4>
+                    <p align="justify" style="text-indent: 25px;">SFAC is fully committed to academic success through quality instruction, purposeful research, sustainable extension services while promoting love of God, family, and country.</p>
+                      </div>
+                </div>
+                <div class="col-xl-4 d-flex align-items-stretch">
+                  <div class="icon-box mt-4 mt-xl-0">
+                    <h4>CORE VALUES</h4>
+                    <p align="justify" style="text-indent: 25px;">Our character is shaped by the life and teachings of our patron, Saint Francis of Assisi, and guided by the enduring values of Faith, Humility, Discipline, and Service.</p>
+                    </div>
+                </div>
+              </div>
+            </div><!-- End .content-->
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End Why Us Section -->
+  </main><!-- End #main -->
+
+<?php include('assets/includes/footer.php') ?>
